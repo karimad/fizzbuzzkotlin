b5dd6ec048df74c342bc5b20d3a9f4cee3118d41
karimad <kmehaleb@gmail.com>
2019-10-10 14:22:24 +0200

    Update FizzBuzz.kt

--- src/FizzBuzz.kt
+++ src/FizzBuzz.kt
@@ -1,5 +1,5 @@
 fun fizzBuzz(N: Int) = (1..N).map {  if (it % 15 == 0) " fizzbuzz " else { if (it % 3 == 0) " fizz " else if (it % 5 == 0) " buzz " else " $it " }}.toString()
 
-fun main(args : Array<String>) {
+fun main() {
     print(fizzBuzz(10))
 }
